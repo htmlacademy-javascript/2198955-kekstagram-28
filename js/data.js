4e6d70a73cbef95ddcc65562414aaa1576752774
Daniil_Prokopenko <element-off@mail.ru>
2023-04-12 15:21:08 +0300

    весь

--- js/data.js
+++ js/data.js
@@ -1,83 +1,74 @@
-import {getRandomSentence, getRandomNumber} from './util.js';
+import { getRandomInt, getRandomElement, getUniqueValue } from './util.js';
 
-// Список возможных предложений для комментариев
-
-const COMMENT_SENTENCES = [
-  'Отличная фотография!',
-  'Классный ракурс!',
-  'Красивая картинка!',
-  'Прекрасное место!',
-  'Хорошо передана атмосфера!',
-  'Очень интересно!',
-  'Очаровательная фотография!',
-  'Какое мастерство!',
-  'Замечательный снимок!',
-  'Очень красиво!',
-  'Удачный момент!',
-  'Супер!',
-  'Как же красиво!',
-  'Великолепно!',
-  'Приятная композиция!',
-  'Супер ракурс!',
-  'Мне нравится!',
-  'Прекрасное фото!',
-  'Хорошее качество!',
-  'Красивые цвета!',
-  'Завораживает!',
-  'Какая атмосфера!',
-  'Отличный фокус!',
-  'Чудесная работа!',
-  'Сильно!'
-];
-
-// Функция для генерации случайного комментария
-const generateComment = () => {
-  const avatar = `img/avatar-${getRandomNumber(1, 6)}.svg`;
-  const message = getRandomSentence(COMMENT_SENTENCES);
-  const name = 'Гость';
-  return { id: getRandomNumber(1, 1000), avatar, message, name };
+const POSTS_DATA = {
+  count_posts: 25,
+  count_random_posts: 10,
+  names: ['Иван', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+  comments: [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  ],
+  count_likes: {
+    min: 15,
+    max: 200,
+  },
+  count_comments: {
+    min: 0,
+    max: 10,
+  },
+  count_avatar: 6,
+  comment_max_length: 140,
 };
 
-// Функция для генерации объектов фотографий
+let posts;
+const arrayIds = [];  // массив идентификаторов комментарий
 
-export const generateComments = (count) => {
+// генерация комментариев
+const createComment = () => {
   const comments = [];
-  const usedCommentIds = new Set();
+  let commentId;
 
-  for (let j = 1; j <= count; j++) {
-    let commentId = getRandomNumber(1, 1000);
-    while (usedCommentIds.has(commentId)) {
-      commentId = getRandomNumber(1, 1000);
-    }
-    usedCommentIds.add(commentId);
-    comments.push({ id: commentId, ...generateComment() });
+  for (let i = 0; i < getRandomInt(POSTS_DATA.count_comments.min, POSTS_DATA.count_comments.max); i++) {
+    commentId = getUniqueValue(arrayIds, 1, 999);
+    arrayIds.push(commentId);
+
+    let messages = new Array(2)                                         // объявляем массив
+      .fill(null)                                                       // присваиваем null всум элементам
+      .map(() => getRandomElement(POSTS_DATA.comments))                 // заполняем случайными значениями
+      .filter((_, index) => index ? getRandomInt(0, 1) : 1)             // оставляем первый элемент (чтоб не был пустым), остальные выводим рандомно
+      .reduce((result, item) => {                                       // удаляем дубликаты
+        return result.includes(item) ? result : [...result, item];
+      }, [])
+      .join(' ');                                                       // склеиваем в строку
+
+    comments.push({
+      id: commentId,
+      avatar: `img/avatar-${getRandomInt(1, POSTS_DATA.count_avatar)}.svg`,
+      message: messages.substr(0, POSTS_DATA.comment_max_length),
+      name: getRandomElement(POSTS_DATA.names),
+    });
   }
 
   return comments;
 };
 
-export const generatePhotos = (count) => {
-  const photos = [];
-  const usedIds = new Set();
-
-  for (let i = 1; i <= count; i++) {
-    let id = getRandomNumber(1, count);
-    while (usedIds.has(id)) {
-      id = getRandomNumber(1, count);
-    }
-    usedIds.add(id);
-
-    const url = `photos/${i}.jpg`;
-
-    const commentsCount = getRandomNumber(0, 10);
-    const comments = generateComments(commentsCount);
-
-    const likes = getRandomNumber(15, 200);
-    const description = getRandomSentence(COMMENT_SENTENCES);
-    photos.push({ id, url, description, likes, comments });
-  }
-
-  return photos;
+// генерация постов
+const createPosts = () => {
+  posts = new Array(POSTS_DATA.count_posts)
+    .fill(null)
+    .map((item, index) => {
+      return {
+        id: index + 1,
+        url: `photos/${index + 1}.jpg`,
+        description: `Описание #${index + 1}`,
+        likes: getRandomInt(15, 200),
+        comments: createComment(),
+      };
+    });
 };
 
-
+export { POSTS_DATA, createPosts, posts };
